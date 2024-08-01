65cdef2f246e3e42075d4d9e05408d87a23a184c
jooa7878 <jooa7878@naver.com>
2024-08-01 13:44:30 +0900

    remove : console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   try {
-    console.log("middleware init");
-
     // API Request
     // const response = await fetch(...)
     throw new Error("test");
